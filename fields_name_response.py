483160baa45d2b0f322f371a792fb77659eddc5e
siracencoserghei <siracencoserghei@gmail.com>
2024-02-02 22:16:17 +0100

    first part refactoring

--- fields_name_response.py
+++ fields_name_response.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import requests
 
 today = datetime.now().date()
-url = f'https://api.privatbank.ua/p24api/exchange_rates?json&date={today.strftime("%d.%m.%Y")}'
+# url = f'https://api.privatbank.ua/p24api/exchange_rates?json&date={today.strftime("%d.%m.%Y")}'
+
+url = f'https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2014'
 result = requests.get(url)
 print(result.json())
--- main.py
+++ main.py
@@ -10,8 +10,12 @@ class Data_exchange_pb:
     BASE_URL = 'https://api.privatbank.ua/p24api/exchange_rates?'
     CURRENCIES = ()
 
-    def format_obj(self, obj):
+    def filter_currencies(self, obj):
         filtered_currencies = list(filter(lambda x: x['currency'] in self.CURRENCIES, obj['exchangeRate']))
+        return filtered_currencies
+
+    async def format_obj(self, obj):
+        filtered_currencies = self.filter_currencies(obj)
         formatted_currencies = {
             obj['date']: {
                 x['currency']: {'sale': x.get('saleRate'), 'purchase': x.get('purchaseRate')}
@@ -36,7 +40,7 @@ class Data_exchange_pb:
         except aiohttp.ClientConnectionError as err:
             logging.info(err)
 
-    async def data_from_api(self, days: List[str]):
+    async def data_from_api(self, days: List[str]):  # days: List[str] - list of days '%d.%m.%Y'
         async with asyncio.TaskGroup() as tg:
             results = []
             for date in days:
@@ -44,21 +48,41 @@ class Data_exchange_pb:
                 results.append(await task)
             return results
 
+    async def get_data(self, days_list):
+        data = await self.data_from_api(days_list)
+        return data
+
+    async def format_results(self, data):
+        results = []
+
+        for day_data in data:
+            result = await self.format_obj(day_data)
+            results.append(result)
+        return results
+
+    async def print_results(self, results):
+        print(json.dumps(results, indent=2))
+
+    async def results_data(self, days_list, currencies):
+        data = await self.get_data(days_list)
+        results = await self.format_results(data)
+        return results
+
     async def main(self):
         parser = argparse.ArgumentParser(description='Fetch exchange rates for specified days')
         parser.add_argument('days', type=int, help='Number of days to fetch exchange rates')
         parser.add_argument('--currencies', '-c', default='EUR,USD', help='Comma-separated list of currencies')
         args = parser.parse_args()
+        days = args.days
+        currencies = args.currencies
+        self.CURRENCIES = tuple(currencies.split(','))
+        days_num = min(abs(days), 10)
+        days_list = await self.get_days_list(days_num)
+        # days_list = ['01.12.2014', '01.12.2023']
+        results = await self.results_data( days_list, currencies)
 
-        days_num = min(abs(args.days), 10)
-        self.CURRENCIES = tuple(args.currencies.split(','))
-        days = await self.get_days_list(days_num)
-        data = await self.data_from_api(days)
-        results = []
-        for day_data in data:
-            result = self.format_obj(day_data)
-            results.append(result)
-        print(json.dumps(results, indent=2))
+        await self.print_results(results)
+
 
 if __name__ == '__main__':
     data = Data_exchange_pb()
